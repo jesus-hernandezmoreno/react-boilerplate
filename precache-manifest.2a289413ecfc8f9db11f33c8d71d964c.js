d94716ab5bd75d7c5d3603ee6c7533bc4db8266d
Jesús Hernández <jesus.hernandezmoreno@outlook.com>
2020-04-24 17:00:48 -0500

    Updates

---
+++ precache-manifest.2a289413ecfc8f9db11f33c8d71d964c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df2514bea47839d7c7d508f40539994",
+    "url": "/react-boilerplate/index.html"
+  },
+  {
+    "revision": "8de3e5b233b883ca6655",
+    "url": "/react-boilerplate/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "79354b8a35cc3308235c",
+    "url": "/react-boilerplate/static/css/main.e9811ed8.chunk.css"
+  },
+  {
+    "revision": "8de3e5b233b883ca6655",
+    "url": "/react-boilerplate/static/js/2.7d2f0568.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/react-boilerplate/static/js/2.7d2f0568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79354b8a35cc3308235c",
+    "url": "/react-boilerplate/static/js/main.0e7016d1.chunk.js"
+  },
+  {
+    "revision": "c4f22c65d5cb28095b6b",
+    "url": "/react-boilerplate/static/js/runtime-main.40c759b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-boilerplate/static/media/logo.5d5d9eef.svg"
+  }
+]);
